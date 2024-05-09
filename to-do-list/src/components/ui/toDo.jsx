a6f363765de4d6f2854a0d3144f6365113f0776f
Your Name <sirajkp752@gmail.com>
2024-05-09 09:09:37 +0530

    first gg

---
+++ to-do-list/src/components/ui/toDo.jsx
@@ -0,0 +1,8 @@
+
+function ToDo({name,des,date,status}) {
+    return (
+        <div style={{ transition: 'all ease 0.3s' }} className="mb-2 w-[98%] max-h-[3rem] h-[3rem] rounded-md bg-zinc-800 hover:bg-zinc-900"></div>
+    )
+}
+
+export default ToDo
